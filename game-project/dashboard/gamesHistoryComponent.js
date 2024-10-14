8ecc0745f9353fff6f77f8ce5d825589704de840
Dominik Wujek <dom.wujek@gmail.com>
2024-10-14 09:41:51 +0100

    games history component

--- game-project/dashboard/gamesHistoryComponent.js
+++ game-project/dashboard/gamesHistoryComponent.js
@@ -1,15 +1,34 @@
 import { pubsub } from "../utilities/pubsub.js";
 import { getUserGames } from "../utilities/gamesHistory.js";
 
-
 export const gamesHistoryComponent = {
     games: getUserGames(),
 
-    render: ()=>{
-        const element = document.querySelector(".gamesHistory")
-        gamesHistoryComponent.games.forEach(element => {
-            console.log(element)
+    render: () => {
+        const element = document.querySelector(".gamesHistory");
+        // element.innerHTML = "";
+
+        gamesHistoryComponent.games.forEach((game, index) => {
+            
+            const gameButton = document.createElement("button");
+            gameButton.classList.add("game-item");
+            
+            gameButton.innerHTML = `
+                <div class="gameInfo">
+                    <h3>Level: ${game.level}</h3>
+                    <p>Date: ${new Date(game.date).toLocaleDateString()}</p>
+                    <p>Score: ${game.overallScore}</p>
+                </div>
+            `;
+            gameButton.addEventListener("click", () => {
+                
+                console.log(`Game ${index + 1} clicked`, game);
+            });
+
+            element.appendChild(gameButton); 
+            
         });
     }
+};
 
-}
+gamesHistoryComponent.render();
