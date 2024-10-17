93f7bc67c70d126c9ad70a40cccc68edefacfd8c
Dominik Wujek <dom.wujek@gmail.com>
2024-10-17 21:47:30 +0100

    interactive regEx dependent audio player setup

--- game-project/register.js
+++ game-project/register.js
@@ -1,6 +1,7 @@
-// import { state } from "./state.js"
 
-console.log("test")
+import regAudio from './utilities/regAudio.js'
+import PitchVisualizer from './utilities/pitch-visualizer.js';
+import updateMeter from './utilities/updateMeter.js';
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
@@ -165,5 +166,88 @@ document.addEventListener("DOMContentLoaded", ()=>{
                                                 regAlert.classList.add("down");
                                                 regStartBtn.classList.add("hidden");
                                             })
+
+
+
+
+
+
+
+
+//player //player //player //player //player //player //player //player 
+
+
+
+
+
+
+
+
+
+const volumeBar = document.getElementById('volume-bar');
+const audioPlayer1 = document.getElementById('player1');
+
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+
+const audioAnalyser = new AudioAnalyser(audioContext);
+
+
+audioAnalyser.init()
+
+updateMeter(audioAnalyser, volumeBar);
+
+
+
+
+
+
+
+// chart ?
+
+google.charts.load('current', {'packages':['gauge']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+      ['', 'Value'],
+      ['', 80],
+      ['', 55],
+      ['', 68]
+    ]);
+
+    var options = {
+      width: 300, height: 120,
+      redFrom: 90, redTo: 100,
+      yellowFrom:75, yellowTo: 90,
+      minorTicks: 5
+    };
+
+    var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+
+    // setInterval(function() {
+    //   data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+    //   chart.draw(data, options);
+    // }, 13000);
+    // setInterval(function() {
+    //   data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+    //   chart.draw(data, options);
+    // }, 200);
+    // setInterval(function() {
+    //   data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
+    //   chart.draw(data, options);
+    // }, 26000);
+
+    setTimeout(()=> {
+        data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+    chart.draw(data, options);
+}, 1000)
+
+  }
+
+
 
 })
