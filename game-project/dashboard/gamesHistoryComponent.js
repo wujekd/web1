2ad2c4abb2e0aa9aaae1bec1d98e3cccdaf92ace
Dominik Wujek <dom.wujek@gmail.com>
2024-10-19 17:37:53 +0100

    showing only games of a selected level

--- game-project/dashboard/gamesHistoryComponent.js
+++ game-project/dashboard/gamesHistoryComponent.js
@@ -3,38 +3,45 @@ import { getGames, getUserGames } from "../utilities/gamesHistory.js";
 import { lvlsComponent } from "./lvlsComponent.js";
 
 export const gamesHistoryComponent = {
-    games: {},
+    games: getUserGames(),
     allGames: getGames(),
     displaying: "",
 
 
     render: () => {
 
-        gamesHistoryComponent.games = getUserGames();
         gamesHistoryComponent.showUserGames();
-
-
 
         const yourGamesBtn = document.getElementById("urGamesbtn")
         const scoreTableBtn = document.getElementById("scoreTableBtn");
         yourGamesBtn.addEventListener("click", gamesHistoryComponent.showUserGames);
         scoreTableBtn.addEventListener("click", gamesHistoryComponent.showScoreTable);
+        const historyLevelsDiv = document.querySelector(".historyLevels");
+        
+        lvlsComponent.lvList.forEach((lvl, index)=>{
+                const btn = document.createElement("button")
+                btn.textContent = `${lvl.id}`
+                btn.classList.add("historyLvlBtn")
+                btn.addEventListener("click", ()=> gamesHistoryComponent.sortGamesBy(Number(btn.textContent)))
+                historyLevelsDiv.appendChild(btn)
+            });
 
         const sortbyScoreBtn = document.getElementById("sortScore");
-        const sortByLvlBtn = document.getElementById("sortLvl");
+        const historyLvlsBtnsDiv = document.getElementById("historyLevels");
         const sortByDateBtn = document.getElementById("sortDate");
         sortbyScoreBtn.addEventListener("click", ()=> gamesHistoryComponent.sortGamesBy("score"));
         sortByDateBtn.addEventListener("click", ()=> gamesHistoryComponent.sortGamesBy("date"));
 
-
-        const historyLevelsDiv = document.querySelector(".historyLevels");
-        lvlsComponent.lvList.forEach((lvl, index)=>{
-                                                    const btn = document.createElement("button")
-                                                    btn.textContent = `${lvl.id}`
-                                                    btn.classList.add("historyLvlBtn")
-                                                    btn.addEventListener("click", ()=>{ console.log(`hey ${lvl.id}`)})
-                                                    historyLevelsDiv.appendChild(btn)
+        const historyLevelsBtns = historyLevelsDiv.querySelectorAll("button");
+        historyLevelsBtns.forEach((btn)=> {
+            btn.addEventListener("click", ()=> gamesHistoryComponent.sortGamesBy(Number(btn.textContent)))
         })
+
+      
+
+
+      
+        
 
 
 
@@ -109,27 +116,29 @@ export const gamesHistoryComponent = {
                     return new Date(a.date).getTime() - new Date(b.date).getTime(); 
                 } else if (what == "score") {
                     return a.overallScore - b.overallScore;
-                } else if (typeof what == number) {
-                    return b.level - a.level; 
+                } else if (typeof what == 'number') {
+                    gamesHistoryComponent.games = gamesHistoryComponent.games.filter(game => game.level == what);
+                    return a.overallScore - b.overallScore;
                 }
                 return 0;
             });
             gamesHistoryComponent.showUserGames();
+            gamesHistoryComponent.games = getUserGames();
 
         } else if (gamesHistoryComponent.displaying == "scoreTable"){
             gamesHistoryComponent.allGames.sort((a, b) => {
                 if (what == "date") {
-                    console.log("date")
                     return new Date(a.date).getTime() - new Date(b.date).getTime(); 
                 } else if (what == "score") {
-                    console.log("score")
                     return a.overallScore - b.overallScore;
-                } else if (typeof what == number) {
-                    return b.level - a.level;  
+                } else if (typeof what == 'number') {
+                    gamesHistoryComponent.allGames = gamesHistoryComponent.allGames.filter(game => game.level == what);
+                    return a.overallScore - b.overallScore;
                 }
                 return 0; 
             });
             gamesHistoryComponent.showScoreTable();
+            gamesHistoryComponent.allGames = getGames();
         }
     }
 };
